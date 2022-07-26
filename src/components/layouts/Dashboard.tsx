c710cc5a82d0c21fac0d664d6b7bddbc88e87454
Ameya Lambat <ameyalambat@gmail.com>
2022-07-26 01:47:40 -0400

    fix:fixed dashboard wailist button

--- src/components/layouts/Dashboard.tsx
+++ src/components/layouts/Dashboard.tsx
@@ -4,6 +4,13 @@
 import { MdOutlineScheduleSend } from "react-icons/md";
 
 const Dashboard = () => {
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <section className="mt-16 w-full overflow-x-hidden">
       <div className="relative max-w-6xl my-0 mx-auto py-10 px-10 lg:px-0 flex justify-center">
@@ -17,7 +24,12 @@ const Dashboard = () => {
               first to try it out!
             </p>
             <div className="py-10">
-              <button className="ring-2 ring-gray-900 hover:bg-cyan-600/10 shadow-md text-sm font-semibold py-2 px-3 rounded-lg flex justify-between items-center">
+              <button
+                onClick={() => {
+                  scrollToBottom();
+                }}
+                className="ring-2 ring-gray-900 hover:bg-cyan-600/10 shadow-md text-sm font-semibold py-2 px-3 rounded-lg flex justify-between items-center"
+              >
                 <span className="pr-2">Join Waitlist</span>
                 <MdOutlineScheduleSend size={20} />
               </button>
